de202c745f60222ec2e99825dd56004de6fd7e98
Flavio W. Brasil <fwbrasil@gmail.com>
2012-01-16 16:36:27 -0200

    .

---
+++ activate-core/src/main/scala/net/fwbrasil/activate/entity/EntityValidation.scala
@@ -0,0 +1,117 @@
+package net.fwbrasil.activate.entity
+
+import net.fwbrasil.radon.ref.RefListener
+import scala.collection.mutable.ListBuffer
+import net.fwbrasil.radon.ref.Ref
+import scala.collection.SeqLike
+
+case class PostCond[R](f: () => R) {
+
+	def postCond(condition: => Boolean): R =
+		postCond(condition, "")
+
+	def postCond(condition: => Boolean, name: String): R = {
+		val result = f()
+		require(condition, name)
+		result
+	}
+
+	def postCond(condition: (R) => Boolean): R =
+		postCond(condition, "")
+
+	def postCond(condition: (R) => Boolean, name: String): R = {
+		val result = f()
+		require(condition(result), name)
+		result
+	}
+
+	private[this] def require(f: => Boolean, name: String) =
+		if (!f)
+			throw new PostCondidionViolationException(name)
+}
+
+case class Invariant(f: () => Boolean)
+
+trait ViolationException extends Exception
+case class InvariantViolationException(violations: String*) extends ViolationException
+case class PreCondidionViolationException(violations: String*) extends ViolationException
+case class PostCondidionViolationException(violations: String*) extends ViolationException
+
+trait ValidEntity {
+	this: Entity =>
+
+	private[activate] lazy val invariants = {
+		initializeListener
+		val metadata = EntityHelper.getEntityMetadata(this.getClass)
+		for(method <- metadata.invariantMethods)
+			yield (method.getName, method.invoke(this).asInstanceOf[Invariant].f)
+	}
+
+	private[this] def initializeListener: Unit = {
+		val listener = new RefListener[Any] with Serializable {
+			override def notifyPut(ref: Ref[Any], obj: Option[Any]) = {
+				validate
+			}
+		}
+		for (ref <- vars)
+			ref.asInstanceOf[Ref[Any]].addWeakListener(listener)
+
+	}
+	protected def invariant(f: => Boolean) =
+		Invariant(() => f)
+
+	protected implicit def toPostCond[R](f: => R) = PostCond(() => f)
+
+	protected def preCond[R](condition: => Boolean)(f: => R): R =
+		preCond[R](condition, "")(f)
+
+	protected def preCond[R](condition: => Boolean, name: String)(f: => R): R = {
+		if (!condition)
+			throw new PreCondidionViolationException(name)
+		f
+	}
+
+	def validate = {
+		val invalid = invalidInvariants
+		if (invalid.nonEmpty)
+			throw new InvariantViolationException(invalid: _*)
+	}
+
+	private[this] def invalidInvariants = {
+		val inv = invariants
+		for ((name, function) <- inv; if (!function()))
+			yield name
+	}
+}
+
+trait ValidEntityContext {
+
+	implicit def toNotNull(string: String) = NotNull(string)
+	case class NotNull(obj: Any) {
+		def notNull =
+			obj != null
+	}
+
+	implicit def toValidSeqLike[S <% SeqLike[_, _]](obj: S) = ValidSeqLike(obj)
+	case class ValidSeqLike[S <% SeqLike[_, _]](obj: S) {
+		def length(value: Int) =
+			obj != null && obj.size == value
+		def maxLength(value: Int) =
+			obj != null && obj.size <= value
+		def minLength(value: Int) =
+			obj != null && obj.size >= value
+		def notBlank =
+			obj != null && obj.nonEmpty
+	}
+
+	implicit def toValidNumeric[N](obj: N)(implicit numeric: Numeric[N]) = ValidNumeric(obj)
+	case class ValidNumeric[N](obj: N)(implicit numeric: Numeric[N]) {
+		def range(a: N, b: N) =
+			numeric.lteq(a, obj) && numeric.lteq(obj, b)
+		def positive =
+			numeric.signum(obj) >= 0
+		def negative =
+			numeric.signum(obj) < 0
+	}
+
+}
---
+++ activate-core/src/main/scala/net/fwbrasil/activate/query/OrderedQuery.scala
@@ -0,0 +1,95 @@
+package net.fwbrasil.activate.query
+
+import org.joda.time.base.AbstractInstant
+
+trait OrderedQueryContext {
+	implicit def abstractInstantOrdering[A <: AbstractInstant]: Ordering[A] = new Ordering[A] {
+		def compare(x: A, y: A) = x.toDate.compareTo(y.toDate)
+	}
+
+	implicit def Tuple1[T1](implicit ord: Ordering[T1]): Ordering[Tuple1[T1]] =
+		new Ordering[Tuple1[T1]] {
+			def compare(x: Tuple1[T1], y: Tuple1[T1]): Int = {
+				ord.compare(x._1, y._1)
+			}
+		}
+
+	implicit def toOrderByCriteria[T <% QuerySelectValue[T]](value: T): OrderByCriteria[T] =
+		OrderByCriteria[T](orderByAscendingDirection, value)
+
+	implicit def toOrderByDirectionWrapper[T <% QuerySelectValue[T]](value: T) =
+		OrderByDirectionWrapper[T](value)
+
+	implicit def toOrderByWrapper[S](query: Query[S]) =
+		OrderByWrapper(query)
+}
+
+case class OrderByWrapper[S](query: Query[S]) {
+
+	def orderBy[T1](value: OrderByCriteria[T1])(implicit ordering: Ordering[Tuple1[T1]]) =
+		orderedQuery(ordering, value)
+
+	def orderBy[T1, T2](
+		value1: OrderByCriteria[T1],
+		value2: OrderByCriteria[T2])(implicit ordering: Ordering[Tuple2[T1, T2]]) =
+		orderedQuery(
+			ordering,
+			value1,
+			value2)
+
+	def orderBy[T1, T2, T3](
+		value1: OrderByCriteria[T1],
+		value2: OrderByCriteria[T2],
+		value3: OrderByCriteria[T3])(implicit ordering: Ordering[Tuple3[T1, T2, T3]]) =
+		orderedQuery(
+			ordering,
+			value1,
+			value2,
+			value3)		
+			
+	def orderBy[T1, T2, T3, T4](
+		value1: OrderByCriteria[T1],
+		value2: OrderByCriteria[T2],
+		value3: OrderByCriteria[T3],
+		value4: OrderByCriteria[T3])(implicit ordering: Ordering[Tuple4[T1, T2, T3, T4]]) =
+		orderedQuery(
+			ordering,
+			value1,
+			value2,
+			value3,
+			value4)				
+			
+	private[this] def orderedQuery(ordering: Ordering[_], values: OrderByCriteria[_]*): Query[S] =
+		OrderedQuery[S](query.from, query.where, query.select, OrderBy(ordering, values: _*))
+}
+
+case class OrderedQuery[S](override val from: From, override val where: Where, override val select: Select, _orderBy: OrderBy)
+		extends Query[S](from, where, select) {
+	override def orderByClause = Some(_orderBy)
+	override def toString = super.toString + _orderBy.toString
+}
+
+case class OrderBy(ordering: Ordering[_], criterias: OrderByCriteria[_]*) {
+	override def toString = " orderBy (" + criterias.mkString(", ") + ")"
+}
+
+abstract sealed class OrderByDirection
+case object orderByAscendingDirection extends OrderByDirection {
+	override def toString = "asc"
+}
+case object orderByDescendingDirection extends OrderByDirection {
+	override def toString = "desc"
+}
+
+case class OrderByDirectionWrapper[T](value: QuerySelectValue[T]) {
+	def asc =
+		OrderByCriteria[T](orderByAscendingDirection, value)
+	def desc =
+		OrderByCriteria[T](orderByDescendingDirection, value)
+}
+
+case class OrderByCriteria[T](direction: OrderByDirection, value: QuerySelectValue[T]) {
+	def this(value: QuerySelectValue[T]) =
+		this(orderByAscendingDirection, value)
+	override def toString = value.toString() + " " + direction.toString
+}
