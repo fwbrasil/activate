d9effe470376eb26fd9586ce76fc88791960f645
Flavio W. Brasil <fwbrasil@gmail.com>
2012-03-09 19:17:38 -0300

    .

---
+++ activate-core/src/main/scala/net/fwbrasil/activate/util/WildcardRegexUtil.scala
@@ -0,0 +1,33 @@
+package net.fwbrasil.activate.util
+
+object WildcardRegexUtil {
+
+	def wildcardToRegex(wildcard: String) = {
+		val s = new StringBuffer(wildcard.length)
+		s.append('^')
+		for (c <- wildcard)
+			c match {
+				case '*' =>
+					s.append(".*")
+				case '?' =>
+					s.append(".")
+				case '(' =>
+				case ')' =>
+				case '[' =>
+				case ']' =>
+				case '$' =>
+				case '^' =>
+				case '.' =>
+				case '{' =>
+				case '}' =>
+				case '|' =>
+				case '\\' =>
+					s.append("\\")
+					s.append(c)
+				case default =>
+					s.append(c)
+			}
+		s.append('$')
+		s.toString
+	}
+}
