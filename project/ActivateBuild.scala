4d58b14a3d258675ec0625df1c50812cc1535365
Flavio W. Brasil <fwbrasil@gmail.com>
2012-08-17 10:20:54 -0300

    1.0-RC3

--- project/ActivateBuild.scala
+++ project/ActivateBuild.scala
@@ -5,7 +5,7 @@ object ActivateBuild extends Build {
   	
 	/* Core dependencies */
   	val javassist = "org.javassist" % "javassist" % "3.16.1-GA" withSources
-	val radonStm = "net.fwbrasil" %% "radon-stm" % "1.0-SNAPSHOT"
+	val radonStm = "net.fwbrasil" %% "radon-stm" % "1.0-RC3"
 	val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
 	val objenesis = "org.objenesis" % "objenesis" % "1.2"
 	val jug = "com.fasterxml.uuid" % "java-uuid-generator" % "3.1.3"
@@ -132,7 +132,7 @@ object ActivateBuild extends Build {
     def commonSettings = 
     	Defaults.defaultSettings ++ Seq(
     		organization := "net.fwbrasil",
-    		version := "1.0-SNAPSHOT",
+    		version := "1.0-RC3",
     		scalaVersion := "2.9.1",
     		crossScalaVersions := Seq("2.9.1", "2.9.2"),
     	    testFrameworks ++= Seq(specs2Framework),
