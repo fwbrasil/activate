a0c56c238c76bbc03a9b359a6dfc2585659c4a6d
Flavio W. Brasil <fwbrasil@gmail.com>
2013-06-02 10:34:48 -0300

    radon 1.2.3

--- project/ActivateBuild.scala
+++ project/ActivateBuild.scala
@@ -5,7 +5,7 @@ object ActivateBuild extends Build {
   	
 	/* Core dependencies */
   	val javassist = "org.javassist" % "javassist" % "3.17.1-GA"
-	val radonStm = "net.fwbrasil" %% "radon-stm" % "1.2.1"
+	val radonStm = "net.fwbrasil" %% "radon-stm" % "1.2.3-SNAPSHOT"
 	val smirror = "net.fwbrasil" %% "smirror" % "0.5"
 	val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
 	val objenesis = "org.objenesis" % "objenesis" % "1.2"
@@ -153,7 +153,7 @@ object ActivateBuild extends Build {
     def commonSettings = 
     	Defaults.defaultSettings ++ Seq(
     		organization := "net.fwbrasil",
-    		version := "1.2.2",
+    		version := "1.2.3-SNAPSHOT",
     		scalaVersion := "2.10.1",
     		crossScalaVersions := Seq("2.10.0", "2.10.1"),
     		javacOptions ++= Seq("-source", "1.5", "-target", "1.5"),
