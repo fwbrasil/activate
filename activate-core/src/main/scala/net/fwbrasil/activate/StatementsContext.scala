9cfeea82d93fc9f05b427fcdec7d78c23236d5fe
Flavio W. Brasil <fwbrasil@gmail.com>
2013-07-29 21:46:07 -0300

    performance

--- activate-core/src/main/scala/net/fwbrasil/activate/StatementsContext.scala
+++ activate-core/src/main/scala/net/fwbrasil/activate/StatementsContext.scala
@@ -13,20 +13,21 @@ trait StatementsContext {
     private val transactionStatements =
         new ReferenceWeakKeyMap[Transaction, ListBuffer[MassModificationStatement]]() with Lockable
 
-    private[activate] def currentTransactionStatements = {
-        val transaction = transactionManager.getRequiredActiveTransaction
-        statementsForTransaction(transaction).getOrElse {
-            transactionStatements.doWithWriteLock {
-                transactionStatements.getOrElseUpdate(transaction, ListBuffer())
-            }
-        }
-    }
+    private[activate] def currentTransactionStatements = 
+        transactionManager.getActiveTransaction.map {
+            transaction =>
+                statementsForTransaction(transaction).getOrElse {
+                    transactionStatements.doWithWriteLock {
+                        transactionStatements.getOrElseUpdate(transaction, ListBuffer())
+                    }
+                }
+        }.getOrElse(ListBuffer())
 
     private[activate] def statementsForTransaction(transaction: Transaction) =
         transactionStatements.doWithReadLock {
             transactionStatements.get(transaction)
         }
-    
+
     private[activate] def executeMassModification(statement: MassModificationStatement) =
         for (normalized <- MassModificationStatementNormalizer.normalize[MassModificationStatement](statement)) {
             liveCache.executeMassModification(normalized)
