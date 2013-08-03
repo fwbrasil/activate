447f97bab0d1cff9789a86c1fb226c3963618b24
Flavio W. Brasil <fwbrasil@gmail.com>
2013-08-03 16:51:51 -0300

    performance

--- activate-prevalent/src/main/scala/net/fwbrasil/activate/storage/prevalent/DirectBufferPool.scala
+++ activate-prevalent/src/main/scala/net/fwbrasil/activate/storage/prevalent/DirectBufferPool.scala
@@ -2,15 +2,15 @@ package net.fwbrasil.activate.storage.prevalent
 
 import java.nio.ByteBuffer
 import java.util.concurrent.LinkedBlockingQueue
-
 import scala.collection.JavaConversions.collectionAsScalaIterable
+import java.nio.ByteOrder
 
 class DirectBufferPool(bufferSize: Int, bufferPoolSize: Int) {
 
     private val pool = new LinkedBlockingQueue[ByteBuffer](bufferPoolSize)
     
     for(i <- 0 until bufferPoolSize)
-        pool.add(ByteBuffer.allocateDirect(bufferSize))
+        pool.add(ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder))
 
     def pop =
         pool.take
