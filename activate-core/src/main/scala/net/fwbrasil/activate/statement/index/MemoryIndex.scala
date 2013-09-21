8590ce3863f2b8b7262b88e321eb542de715b0b5
Flavio W. Brasil <fwbrasil@gmail.com>
2013-09-20 23:18:34 -0300

    better memory index performance

--- activate-core/src/main/scala/net/fwbrasil/activate/statement/index/MemoryIndex.scala
+++ activate-core/src/main/scala/net/fwbrasil/activate/statement/index/MemoryIndex.scala
@@ -2,7 +2,6 @@ package net.fwbrasil.activate.statement.index
 
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentSkipListSet
-
 import scala.annotation.implicitNotFound
 import scala.collection.JavaConversions.asScalaSet
 import scala.collection.JavaConversions.bufferAsJavaList
@@ -10,7 +9,6 @@ import scala.collection.JavaConversions.collectionAsScalaIterable
 import scala.collection.JavaConversions.mapAsScalaConcurrentMap
 import scala.collection.JavaConversions.setAsJavaSet
 import scala.collection.mutable.ListBuffer
-
 import net.fwbrasil.activate.ActivateContext
 import net.fwbrasil.activate.entity.Entity
 import net.fwbrasil.activate.entity.LazyList
@@ -19,6 +17,7 @@ import net.fwbrasil.activate.util.ManifestUtil.erasureOf
 import net.fwbrasil.radon.transaction.NestedTransaction
 import net.fwbrasil.radon.transaction.Transaction
 import net.fwbrasil.scala.UnsafeLazy._
+import scala.collection.mutable.HashMap
 
 case class MemoryIndex[E <: Entity: Manifest, T] private[index] (
     name: String, keyProducer: E => T, context: ActivateContext)
@@ -26,38 +25,60 @@ case class MemoryIndex[E <: Entity: Manifest, T] private[index] (
 
     import context._
 
-    private val index = new ConcurrentHashMap[T, ConcurrentSkipListSet[String]]()
+    private val index = new HashMap[T, Set[String]]()
+    private val invertedIndex = new HashMap[String, T]()
     val entityClass = erasureOf[E]
 
     private val lazyInit = unsafeLazy(reload)
 
-    def get(key: T) = {
-        lazyInit.get
-        val fromLiveCache =
-            context.liveCache.fromCache(entityClass)
-                .filter(e => e.isDirty && keyProducer(e) == key)
-                .map(_.id)
-        val fromIndex = index.get(key)
-        val ids =
-            if (fromIndex == null)
-                fromLiveCache
-            else
-                fromLiveCache ++ fromIndex
-        new LazyList(ids)
-    }
+    def get(key: T, considerDirtyEntities: Boolean = true) =
+        synchronized {
+            context.transactionManager.getRequiredActiveTransaction
+            lazyInit.get
+            val fromLiveCache =
+                if (considerDirtyEntities)
+                    context.liveCache.fromCache(entityClass)
+                        .filter(e => e.isDirty && keyProducer(e) == key)
+                        .map(_.id)
+                else
+                    List()
+            val ids =
+                index.get(key)
+                    .map(_ ++ fromLiveCache)
+                    .getOrElse(fromLiveCache)
+                    .toList
+            new LazyList(ids)
+        }
 
     private[index] def deleteEntities(entities: List[Entity]): Unit =
-        deleteEntities(entities.map(_.id).toSet)
+        synchronized {
+            deleteEntities(entities.map(_.id).toSet)
+        }
 
     private[index] def deleteEntities(ids: Set[String]) =
-        index.values.foreach(_.removeAll(ids))
+        synchronized {
+            for (id <- ids) {
+                invertedIndex.get(id).map {
+                    key =>
+                        index.get(key).map {
+                            values =>
+                                index.put(key, values -- Set(id))
+                        }
+                        invertedIndex.remove(id)
+                }
+            }
+        }
 
-    private[index] def updateEntities(entities: List[Entity], delete: Boolean = true) = {
-        if (delete) deleteEntities(entities)
-        for (entity <- entities)
-            index.getOrElseUpdate(keyProducer(entity.asInstanceOf[E]), new ConcurrentSkipListSet[String])
-                .add(entity.id)
-    }
+    private[index] def updateEntities(entities: List[Entity], delete: Boolean = true) =
+        synchronized {
+            if (delete) deleteEntities(entities)
+            for (entity <- entities) {
+                val key = keyProducer(entity.asInstanceOf[E])
+                val value = index.getOrElseUpdate(key, Set())
+                index.put(key, value ++ Set(entity.id))
+                invertedIndex.put(entity.id, key)
+            }
+        }
 
     private[index] def reload = {
         info(s"Reloading index $name")
