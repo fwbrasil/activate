087f677a0a8d2b069ca5087ea749d868ac9cff55
Flavio Wionn Brasil <fwbrasil@gmail.com>
2013-11-30 14:49:37 +0100

    1.5-M1

--- project/ActivateBuild.scala
+++ project/ActivateBuild.scala
@@ -4,8 +4,8 @@ import Keys._
 object ActivateBuild extends Build {
   	
 	/* Core dependencies */
-  	val javassist = "org.javassist" % "javassist" % "3.18.0-GA"
-	val radonStm = "net.fwbrasil" %% "radon-stm" % "1.5-SNAPSHOT"
+  val javassist = "org.javassist" % "javassist" % "3.18.0-GA"
+	val radonStm = "net.fwbrasil" %% "radon-stm" % "1.5-M1"
 	val smirror = "net.fwbrasil" %% "smirror" % "0.7-SNAPSHOT"
 	val guava = "com.google.guava" % "guava" % "15.0"
 	val objenesis = "org.objenesis" % "objenesis" % "2.0"
@@ -262,7 +262,7 @@ object ActivateBuild extends Build {
     def commonSettings = 
     	Defaults.defaultSettings ++ Seq(
     		organization := "net.fwbrasil",
-    		version := "1.5-SNAPSHOT",
+    		version := "1.5-M1",
     		scalaVersion := "2.10.1",
     		javacOptions ++= Seq("-source", "1.5", "-target", "1.5"),
     	    publishMavenStyle := true,
