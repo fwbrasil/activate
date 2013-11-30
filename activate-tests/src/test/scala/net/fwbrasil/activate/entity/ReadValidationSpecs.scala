b0bb45797af76c0e057849880ffacb0e28e9bb19
Flavio Wionn Brasil <fwbrasil@gmail.com>
2013-11-30 14:32:52 +0100

    minor

--- activate-tests/src/test/scala/net/fwbrasil/activate/entity/ReadValidationSpecs.scala
+++ activate-tests/src/test/scala/net/fwbrasil/activate/entity/ReadValidationSpecs.scala
@@ -4,15 +4,16 @@ import scala.concurrent.duration.DurationInt
 import org.junit.runner.RunWith
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.runner.JUnitRunner
-import net.fwbrasil.activate.ActivateContext
+import net.fwbrasil.activate.StoppableActivateContext
 import net.fwbrasil.activate.entity.id.UUID
 import net.fwbrasil.activate.multipleVms.JvmFork
 import net.fwbrasil.activate.postgresqlContext
 import org.joda.time.DateTimeUtils
 import org.joda.time.DateTime
 import scala.concurrent.duration._
+import net.fwbrasil.activate.ActivateTest
 
-object readValidationContext extends ActivateContext {
+object readValidationContext extends StoppableActivateContext {
     object versionMigration extends ManualMigration {
         def up = {
             removeAllEntitiesTables.cascade.ifExists
@@ -25,8 +26,20 @@ object readValidationContext extends ActivateContext {
 
     lazy val storage = postgresqlContext.storage
 
-    versionMigration.execute
+    run {
+        versionMigration.execute
+    }
+
+    def run[A](f: => A) = {
+        start
+        try
+            f
+        finally
+            stop
+    }
 }
+
+import readValidationContext._
 
 class ReadValidationEntity(var intValue: Int) extends Entity {
     override def shouldValidateRead = ReadValidationEntity.shouldValidateRead()
@@ -36,10 +49,8 @@ object ReadValidationEntity {
     var shouldValidateRead: () => Boolean = () => true
 }
 
-import readValidationContext._
-
 @RunWith(classOf[JUnitRunner])
-class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
+class ReadValidationSpecs extends ActivateTest {
 
     override def intToRichLong(v: Int) = ???
 
@@ -52,13 +63,13 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
 
         "maintain the lastVersionValidation" in {
 
-            "on create" in fork {
+            "on create" in test {
                 setTime(1)
                 val entity = transactional(new ReadValidationEntity(0))
                 require(entity.lastVersionValidation.equals(DateTime.now))
             }
 
-            "on update" in fork {
+            "on update" in test {
                 setTime(1)
                 val entity = transactional(new ReadValidationEntity(0))
                 setTime(2)
@@ -66,7 +77,7 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
                 require(entity.lastVersionValidation.equals(DateTime.now))
             }
 
-            "on delete" in fork {
+            "on delete" in test {
                 setTime(1)
                 val entity = transactional(new ReadValidationEntity(0))
                 setTime(2)
@@ -74,7 +85,7 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
                 require(entity.lastVersionValidation.equals(DateTime.now))
             }
 
-            "on read validation" in fork {
+            "on read validation" in test {
                 setTime(1)
                 val entity = transactional(new ReadValidationEntity(0))
                 setTime(2)
@@ -84,7 +95,7 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
             }
         }
 
-        "defer read validation for 1 milis" in fork {
+        "defer read validation for 1 milis" in test {
             setTime(1)
             val entity = transactional(new ReadValidationEntity(0))
             deferReadValidationFor(1 millis, entity)
@@ -93,7 +104,7 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
             require(entity.shouldValidateRead)
         }
 
-        "defer read validation for the infinite" in fork {
+        "defer read validation for the infinite" in test {
             setTime(1)
             val entity = transactional(new ReadValidationEntity(0))
             deferReadValidationFor(Duration.Inf, entity)
@@ -102,7 +113,7 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
             require(!entity.shouldValidateRead)
         }
 
-        "reload the entity if necessary" in fork {
+        "reload the entity if necessary" in test {
             val oldValue = 0
             val newValue = 1
             setTime(1)
@@ -112,7 +123,7 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
             require(transactional(entity.intValue) == newValue)
         }
 
-        "reload the entity if necessary after the deferred read time" in fork {
+        "reload the entity if necessary after the deferred read time" in test {
             val oldValue = 0
             val newValue = 1
             setTime(1)
@@ -140,11 +151,12 @@ class ReadValidationSpecs extends SpecificationWithJUnit with Serializable {
     def setTime(milis: Long) =
         DateTimeUtils.setCurrentMillisFixed(milis)
 
-    def fork[R: Manifest](f: => R) = {
-        JvmFork.forkAndExpect(others = List(optimisticOfflineLockingOption, optimisticOfflineLockingValidateReadOption)) {
-            f
-            1
-        }
+    def test[R: Manifest](f: => R) = {
+        if (super.contexts.find(_.storage == readValidationContext.storage).isDefined)
+            JvmFork.forkAndExpect(others = List(optimisticOfflineLockingOption, optimisticOfflineLockingValidateReadOption)) {
+                readValidationContext.run(f)
+                1
+            }
         ok
     }
 }
