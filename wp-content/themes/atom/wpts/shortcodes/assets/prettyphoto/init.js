2e778c247aee2fe0aaca1475bb95dec5fc86b694
Flavio Brasil <fbrasil@twitter.com>
2016-02-27 12:57:37 -0800

    import website

---
+++ wp-content/themes/atom/wpts/shortcodes/assets/prettyphoto/init.js
@@ -0,0 +1,4 @@
+
+jQuery(document).ready(function($){
+    $("a[rel^='prettyPhoto']").prettyPhoto();
+});
---
+++ xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://activate-framework.org</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://activate-framework.org/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://activate-framework.org/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://activate-framework.org/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://activate-framework.org/xmlrpc.php" />
+      <api name="Atom" blogID="" preferred="false" apiLink="http://activate-framework.org/wp-app.php/service" />
+    </apis>
+  </service>
+</rsd>
